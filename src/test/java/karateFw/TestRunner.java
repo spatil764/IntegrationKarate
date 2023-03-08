b7919a2def4909c1a893b4b269b942c607e28c71
spatil764 <sejal.patil@cumminscollege.in>
2023-03-08 16:14:51 +0530

    https://github.com/spatil764/IntegrationKarate.git

---
+++ src/test/java/karateFw/TestRunner.java
@@ -0,0 +1,19 @@
+package karateFw;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import com.intuit.karate.junit4.Karate;
+import org.junit.Test;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+@RunWith(Karate.class)
+@CucumberOptions(features = "classpath:karateFw")
+public class TestRunner 
+{
+	
+	
+
+ 
+}
---
+++ target/test-classes/karateFw/userDetails.feature
@@ -0,0 +1,6 @@
+Feature: fetching User Details
+Scenario: testing the get call for User Details
+ 
+Given url 'https://reqres.in/api/users/2'
+When method GET
+Then status 200
